5b897fb4fd4e127fcb264a387a3c19f794e4a1cb
tikhon-kodland <rearztyads@gmail.com>
2026-01-08 19:16:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot.types import ReactionTypeEmoji
 import random
-from bot_logic import generate_password
+import os
 
 # Замени 'TOKEN' на токен твоего бота
 # Этот токен ты получаешь от BotFather, чтобы бот мог работать
@@ -11,23 +11,11 @@ bot = telebot.TeleBot("8563550318:AAEBOMVGhgZC6pt4mRKlDt5-af8rZYp3xkQ")
 def send_welcome(message):
     bot.reply_to(message, "Привет! Я твой Telegram бот.\nНапиши что-нибудь!")
 
-@bot.message_handler(commands=['hello'])
-def send_hello(message):
-    bot.reply_to(message, "Привет! Как дела?")
-
-@bot.message_handler(commands=['bye'])
-def send_bye(message):
-    bot.reply_to(message, "Пока! Удачи!")
-
-@bot.message_handler(commands=['genpass'])
-def gen_pass(message):
-    password = generate_password(10)
-    bot.reply_to(message, password)
-
-@bot.message_handler(func=lambda message: True)
-def send_reaction(message):
-    emo = ["\U0001F525", "\U0001F917", "\U0001F60E"]  # or use ["🔥", "🤗", "😎"]
-    bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    imgs = os.listdir(r'C:\Users\rearzty\Desktop\my_project\meme_bot\images')
+    with open(fr'C:\Users\rearzty\Desktop\my_project\meme_bot\images\{random.choice(imgs)}', 'rb') as f:  
+        bot.send_photo(message.chat.id, f)
 
 
 bot.polling()
